5d8d9423bc87a0f44634b46a4804846b67c27f77
Rodrigo Orem <8618619+rodorgas@users.noreply.github.com>
2018-10-14 14:11:57 -0300

    Não tenta logar se encontrar msg de erro

--- userscript.js
+++ userscript.js
@@ -7,20 +7,38 @@
 // @match        https://paca.ime.usp.br/*
 // ==/UserScript==
 
-(function() {
-  'use strict';
-  let onLogin = location.href == 'https://paca.ime.usp.br/login/index.php';
+'use strict';
+let loginURL = 'https://paca.ime.usp.br/login/index.php';
 
-  if (onLogin) {
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+if (location.href == loginURL) {
+  const alerta = document.querySelector('div.alert.alert-danger');
+  if (!alerta) {
+    alert('oi');
+    eventFire(document.getElementById('body'), 'click');
+
     setTimeout(function() {
-      document.querySelector('#loginbtn').click()
+      //alert('oi');
+
+      alert(document.querySelector('#password').value);
+      //document.querySelector('#password').click();
+      //document.querySelector('#loginbtn').click();
     }, 1000);
   }
-  else {
-    let deslogado = document.querySelector('.login').innerText
-      .includes('Você ainda não se identificou');
+}
+else {
+  let deslogado = document.querySelector('.login').innerText
+    .includes('Você ainda não se identificou');
 
-    if (deslogado)
-      location.href = 'https://paca.ime.usp.br/login/index.php';
-  }
-})();
+  if (deslogado)
+    location.href = 'https://paca.ime.usp.br/login/index.php';
+}
